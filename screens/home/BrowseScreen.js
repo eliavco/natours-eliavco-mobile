249ed594fca67ac31954f337457348294e0dc45f
Eliav <eliav.s.cohen@gmail.com>
2019-11-25 20:52:59 +0200

    25/11/2019 8:49PM

--- screens/home/BrowseScreen.js
+++ screens/home/BrowseScreen.js
@@ -21,7 +21,9 @@ export default class BrowseScreen extends Component {
             DATA: this.load(),
             refreshing: false,
         };
-        props.navigation.push('Details', { itemId: '5c88fa8cf4afda39709c295a' }); // TODO: Delete in prod
+        if (__DEV__) {
+            props.navigation.push('Details', { itemId: '5c88fa8cf4afda39709c295a' });
+        }
     }
 
     // Update STATE
