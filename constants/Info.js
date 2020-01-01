d45fba7a206842d2a3e8227ec9cc6f1873c2d4b6
Eliav <eliav.s.cohen@gmail.com>
2020-01-02 01:00:34 +0200

    2/1/2020 1:00AM

--- constants/Info.js
+++ constants/Info.js
@@ -1,7 +1,7 @@
 import MapStyle from './../data/mapStyle';
 
 export default {
-    websiteHost: 'https://natours-eliav.herokuapp.com',
+    websiteHost: 'https://eliav.herokuapp.com',
     apiVersion: 'v1',
     mapStyle: MapStyle,
 };
